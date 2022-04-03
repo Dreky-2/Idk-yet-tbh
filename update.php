7b2c6140ff6354cb1aafcd36e429533a685677d3
Dreky <38798094+Dreky-2@users.noreply.github.com>
2022-04-03 23:30:04 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"version":"2.3","filename":"lipp23.png"}
+{"version":"3.30","filename":"lipp24.png","bot_version":"1.5","download_link":"https://github.com/enduity/rPlaceRevamp/archive/refs/tags/v1.5.zip"}
